802c9324ef59e1837c1242ad4ec7d22cc49ef211
Randy Barlow <randy@electronsweatshop.com>
2017-02-08 14:11:01 -0500

    Update

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,26 +1,23 @@
 package org.openshift;
-import
-import
-import
-import
-java.sql.Connection;
-java.sql.DriverManager;
-java.sql.ResultSet;
-java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class InsultGenerator {
-public String generateInsult() {
-String vowels = "AEIOU";
-String article = "an";
-String theInsult = "";
-try {
-String databaseURL = "jdbc:postgresql://";
-databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
-databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
-String username = System.getenv("POSTGRESQL_USER");
-String password = System.getenv("PGPASSWORD");
-Connection connection = DriverManager.getConnection(databaseURL, username,
-password);
-if (connection != null) {
+    public String generateInsult() {
+        String vowels = "AEIOU";
+        String article = "an";
+        String theInsult = "";
+        try {
+            String databaseURL = "jdbc:postgresql://";
+            databaseURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+            databaseURL += "/" + System.getenv("POSTGRESQL_DATABASE");
+            String username = System.getenv("POSTGRESQL_USER");
+            String password = System.getenv("PGPASSWORD");
+            Connection connection = DriverManager.getConnection(databaseURL, username,
+            password);
+            if (connection != null) {
 String SQL = "select a.string AS first, b.string AS second, c.string AS noun
 from short_adjective a , long_adjective b, noun c ORDER BY random() limit 1";
 Statement stmt = connection.createStatement();
